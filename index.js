5741c9c766363e96898c9354776ef910bb84c6d8
githinjisamson1 <githinjisamson148@gmail.com>
2023-10-05 14:02:59 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,56 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+
+  // iterate array
+  for (let i = 0; i < array.length; i++) {
+    // assign item at position i to firstNum for each iteration
+    const firstNum = array[i];
+
+    // less firstNum from target to get suppossed other num
+    const secondNum = target - firstNum;
+
+    // iterate rest of array/starting at one index higher than i
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] === secondNum) {
+        // secondNum confirmed
+        return true;
+      }
+    }
+  }
+
+  // no random pair summed results to target
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+
+  since the algorithm contains a nested loop, 
+  O(n*n) => O(n^2)
+  
+  Big O time complexity, O(n^2)
+
 */
 
 /* 
   Add your pseudocode here
+  IF result of adding any two random numbers in array is equal to target
+    return true
+  ELSE result of adding any two random numbers in array is not equal to targe
+    return false
 */
 
 /*
   Add written explanation of your solution here
+
+  I have defined a function hasTargetSum that takes an array and target as parameters
+  I iterated array using a for loop construct
+  I assigned item at position i for each iteration to firstNum variable
+  I assummed the secondNum would be target minus firstNum
+  I iterated the rest of the array starting an index higher than current i
+  I compared item at position j to secondNum with aid of strict equality
+  if the comparison evaluates to true, true is returned
+  otherwise false is returned
 */
 
 // You can run `node index.js` to view these console logs
